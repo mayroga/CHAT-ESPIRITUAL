0386d57a60206df82624c9a8b5918c64334b17f0
mayroga <132472344+mayroga@users.noreply.github.com>
2026-05-25 14:55:52 -0700

    Delete migrate_db.py

--- migrate_db.py
+++
@@ -1,7 +0,0 @@
-from app.main import create_app 
-from app.models import db
-
-app = create_app()
-with app.app_context():
-    db.create_all()
-    print("✅ Base de datos creada/migrada correctamente")
